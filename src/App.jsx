1305ccc8a94534a76875b7ff8bc2b127a50270d8
Ismael López <ismaellopez.dev@gmail.com>
2024-03-19 23:06:44 -0600

    [add] useState hook example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './styles.scss';
-import Board from './components/Board';
+import { useState } from 'react';
 
 function App() {
+  let [counter, setCounter] = useState(1);
+  console.log('Hello');
+
+  const onBtnClick = () => {
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
   return (
     <div className="app">
-      <Board />
+      <div>
+        <button onClick={onBtnClick}>Click me please</button>
+        <div>{counter}</div>
+      </div>
     </div>
   );
 }
